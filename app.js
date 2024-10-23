5a2603ba8b7504dbde35b3c78d39338092ca84ab
Bijay B.K. <bijaybk307@gmail.com>
2024-10-24 00:30:59 +0545

    #express format

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => res.send('Hello World'));
+const port = process.env.PORT || 8082;
+app.listen(port, ()=> console.log(`Server running on Port ${port}`));
