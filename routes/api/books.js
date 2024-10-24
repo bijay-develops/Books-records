35fdcb2678bc0dfde7e6cc33687e5f7429395f2c
Bijay B.K. <bijaybk307@gmail.com>
2024-10-24 21:27:05 +0545

    Rest apis introduction 1

--- routes/api/books.js
+++ routes/api/books.js
@@ -2,3 +2,13 @@ const express = require('express');
 const router = express.Router(); 
 
 //loading book Model 
+const book = require('../../models/Books');
+
+// @route   Get api/books/test
+// @desc    Tests books route
+// @access  Public
+router.get('/test', (req, res) => res.send('book route testing!'));
+
+
+// @route   Get api/books
+// @desc 
